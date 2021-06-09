fdf718fb6e2656d6aa9e256ff7701c91ad9d0436
Ganline <hayak.311.gungam.8810@gmail.com>
2021-06-09 16:26:10 +0900

    stage11 first

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -3,7 +3,9 @@
 public class Jump : MonoBehaviour
 {
     // ジャンプする力（上向きの力）を定義
-    [SerializeField] private float jumpForce = 20.0f;
+    [SerializeField] private float jumpForcex = 0;
+    [SerializeField] private float jumpForcey = 20.0f;
+    [SerializeField] private float jumpForcez = 0;
 
     /// <summary>
     /// Colliderが他のトリガーに入った時に呼び出される
@@ -15,7 +17,7 @@ public class Jump : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             // 当たった相手のRigidbodyコンポーネントを取得して、上向きの力を加える
-            other.gameObject.GetComponent<Rigidbody>().AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(jumpForcex, jumpForcey, jumpForcez, ForceMode.Impulse);
         }
     }
 }
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour
 {
-  void Update()
-  {
-    transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
-  }
+    [SerializeField] private float rotatex = 15;
+    [SerializeField] private float rotatey = 30;
+    [SerializeField] private float rotatez = 45;
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotatex, rotatey, rotatez) * Time.deltaTime);
+    }
 }
