7088f4df15de7ba86abed2a1db119e55f86bad32
Ganline <hayak.311.gungam.8810@gmail.com>
2021-07-03 17:32:03 +0900

    joystick made

---
+++ Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedJoystick : Joystick
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
+
+    public void FixedUpdate()
+    {
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,47 +5,43 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-		public float speed; // 動く速さ
+    public float speed; // 動く速さ
     public Text timeText; // タイムアタックの UI
-		public Text goalText;//ゴール表示の	UI
-		public Text dropText;//落下した時の処理　UI
+    public Text goalText;//ゴール表示の	UI
+    public Text dropText;//落下した時の処理　UI
+    [SerializeField] public FloatingJoystick joystick;
 
     private Rigidbody rb; // Rididbody
-		private float time; //　タイムアタック用
-		private GoalManager goal;　//goalmanagerを呼び出す
-  // Start is called before the first frame update
-  void Start()
-  {
+    private float time; //　タイムアタック用
+    private GoalManager goal; //goalmanagerを呼び出す
+                              // Start is called before the first frame update
+    void Start()
+    {
 
-		// Rigidbody を取得
-		rb = GetComponent<Rigidbody>();
-		// UI を初期化
-    time = 0.0f;
+        // Rigidbody を取得
+        rb = GetComponent<Rigidbody>();
+        // UI を初期化
+        time = 0.0f;
 
-		timeText.text = "";
-		goalText.text = "";
-		dropText.text = "";
-		//インスタンス生成
-		goal = gameObject.AddComponent<GoalManager>();
-  }
+        timeText.text = "";
+        goalText.text = "";
+        dropText.text = "";
+        //インスタンス生成
+        goal = gameObject.AddComponent<GoalManager>();
+    }
 
-  // Update is called once per frame
-  void FixedUpdate()
-  {
-		// カーソルキーの入力を取得
-		var moveHorizontal = Input.GetAxis("Horizontal");
-		var moveVertical = Input.GetAxis("Vertical");
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // カーソルキーの入力を取得
+        Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
 
-		// カーソルキーの入力に合わせて移動方向を設定
-		var movement = new Vector3(moveHorizontal, 0, moveVertical);
-
-		// Ridigbody に力を与えて玉を動かす
-	 	rb.AddForce(movement * speed);
-
-		//タイムアタック
-		if(goal.isGoal == false){
-			time += Time.deltaTime;
-    	timeText.text = time.ToString("F2");
-		}
-  }
+        //タイムアタック
+        if (goal.isGoal == false)
+        {
+            time += Time.deltaTime;
+            timeText.text = time.ToString("F2");
+        }
+    }
 }
