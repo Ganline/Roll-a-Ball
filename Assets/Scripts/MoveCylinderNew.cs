6df3580a71086adf84759d62f4910b43b2617e53
Ganline <hayak.311.gungam.8810@gmail.com>
2021-07-31 15:06:43 +0900

    allstage make

---
+++ Assets/Scripts/MoveCylinderNew.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCylinderNew : MonoBehaviour
+{
+
+    private Rigidbody rigid;
+    private Vector3 defaultPos;
+
+    [SerializeField] private float timex = 2;
+    [SerializeField] private float widthx = 4;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        defaultPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+
+        rigid.MovePosition(new Vector3(defaultPos.x + Mathf.PingPong(Time.time * timex, widthx), defaultPos.y, defaultPos.z));
+    }
+}
